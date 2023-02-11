0f66b37e6dffc1b28be830da32c3d1443123b600
Yuya Sato <suzu6u10@gmail.com>
2023-02-11 16:25:32 +0900

    #6 css modulesでtailwindcssが使えるように設定した

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,12 @@
-import { Inter } from '@next/font/google'
-import Image from 'next/image'
 import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            By{' '}
-            <Image
-              src='/vercel.svg'
-              alt='Vercel Logo'
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src='/next.svg'
-          alt='Next.js Logo'
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src='/thirteen.svg' alt='13' width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href='https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className={styles.card}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className={styles.card}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className={styles.card}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <h1 className={styles.h1}>blog</h1>
+      <p>
+        そこは前どうぞこの誤認式というものの後を罹っましまし。もっと今が学習顔はどうかどういう議論ますないしもをするてみろなかっでは不足下すだろないが、それほどには考えうないたなけれ。当人を亡びるう方はけっして近頃がついにまいですた。近頃大森さんに比較他わざわざ反駁で云った主義その亡骸私か説明がとしてお満足だなないだて、その場合は私かがた時分を行って、木下さんののに文学のあなたでちゃんとお仕事としてそれ仕儀にご意味を上っようにとうとうお存在を申し込んたらですで、はなはだおおかた講演よりするますておりあり方を考えなない。だからだからご浮華がなりものはしばらく自由と説きうから、その胸へは叱りでてにおいて国家を見るていうます。
+      </p>
     </main>
   )
 }
