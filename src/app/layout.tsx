b4593c69755845ba145a5159cf70f9c095849048
Yuya Sato <suzu6u10@gmail.com>
2023-02-13 09:24:21 +0900

    #7 Add Header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css'
+import Header from '@/components/Header'
 import Head from './head'
 
 export default function RootLayout({
@@ -13,7 +14,14 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <Head />
-      <body>{children}</body>
+      <body>
+        <div className='mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0'>
+          <div className='flex h-screen flex-col justify-between'>
+            <Header />
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
---
+++ src/data/siteMetadata.ts
@@ -0,0 +1,6 @@
+const siteMetadata = {
+  title: 'suzu6-blog',
+  language: 'ja',
+}
+
+export default siteMetadata
